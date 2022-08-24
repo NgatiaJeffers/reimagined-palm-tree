4711d86450bf5a40ccd9f483822cffbad1f129a6
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-08-24 17:27:02 +0300

    JG[feat] Include sponsors and News & updates sections

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Head from 'next/head';
-import Script from 'next/script'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Image from 'next/image.js';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import Banner from "../components/Banner.js";
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
+import CancerScreening from "../public/assets/images/cancer-screening.jpg";
+import FarajaCoffee from "../public/assets/images/coffee.jpg";
 
 export default function Home() {
   return (
@@ -33,7 +37,7 @@ export default function Home() {
             <div className='col-md d-flex justify-content-center counter-wrap'>
               <div className='block-18 color-2 align-items-stretch'>
                 <div className='text'>
-                  <h3 className='mb-4 donate-title'>Donate Money</h3>
+                  <h3 className='mb-4 donate-title'>Donate</h3>
                   <p className='donate-text'>Even the all-powerful Pointing has no control about the blind texts.</p>
               	  <p><a href="#" className="btn btn-white px-3 py-2 mt-2">Donate Now</a></p>
                 </div>
@@ -45,6 +49,163 @@ export default function Home() {
                   <h3 className="mb-4 volunteer-title">Be a Volunteer</h3>
                   <p className='volunteer-text'>Even the all-powerful Pointing has no control about the blind texts.</p>
                   <p><a href="#" className="btn btn-white px-3 py-2 mt-2">Be A Volunteer</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='ftco-section'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-6 d-flex'>
+              <div className='img img-about align-self-stretch'></div>
+            </div>
+            <div className='col-md-6 pl-md-5'>
+              <h2 className='mb-4'>Who we are?</h2>
+              <p>
+                The Faraja Cancer Support Trust was founded in 2010 with the aim of providing emotional, 
+                practical and healing support to anyone affected by cancer. We offer cancer patients 
+                and their carers information, advice, counselling and complementary therapies 
+                in order to make their cancer journey a little more manageable.
+              </p>
+              <p>
+                We work alongside several institutions and hospitals that offer conventional cancer 
+                treatments such as radiotherapy, chemotherapy and surgery. 
+                We aim to take our patients beyond medical treatment through offering them a 
+                holistic approach to coping with the challenges of cancer.
+              </p>
+              <p><a href="#">Read More  <FontAwesomeIcon icon={faAngleRight} /></a></p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Sponsors Section */}
+      <section className='ftco-section'>
+        <div className='container'>
+          <div className='row justify-content-center mb-5 pd-2'>
+            <div className='col-md-7 sponsors-section text-center'>
+              <h2 className='mb-4'>Our Sponsors</h2>
+              <p>
+                To become a better you, remember to be grateful to 
+                people who have contributed to making you who you are today.
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-2 d-flex justify-content-center mb-sm-4">
+              <div className="sponsor">
+                <div className="d-flex mb-4 text-center">
+                  <div className="img"></div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-2 d-flex justify-content-center mb-sm-4">
+              <div className="sponsor">
+                <div className="d-flex mb-4 text-center">
+                  <div className="img1"></div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-2 d-flex justify-content-center mb-sm-4">
+              <div className="sponsor">
+                <div className="d-flex mb-4 text-center">
+                  <div className="img2"></div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-2 d-flex justify-content-center mb-sm-4">
+              <div className="sponsor">
+                <div className="d-flex mb-4 text-center">
+                  <div className="img3"></div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-2 d-flex justify-content-center mb-sm-4">
+              <div className="sponsor">
+                <div className="d-flex mb-4 text-center">
+                  <div className="img4"></div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-2 d-flex justify-content-center mb-sm-4">
+              <div className="sponsor">
+                <div className="d-flex mb-4 text-center">
+                  <div className="img5"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='section-padding'>
+        <div className='news-area'>
+          <div className='container'>
+            <div className='row justify-content-center'>
+              <div className='col-md-12 text-center'>
+                <h1 className='mb-4'>
+                  <span>News & Updates</span>
+                </h1>
+              </div>
+            </div>
+            <div className='row'>
+              <div className='col-md-5'>
+                <div className='news-active'>
+                  <div className='single-blog d-flex align-items-center'>
+                    {/* <div className='thumb'>
+                      <Image 
+                        src={CancerScreening} 
+                        alt='Cancer Screening'
+                        // width="500px"
+                        // height="600px"
+                        // placeholder="blur"  
+                      />
+                    </div> */}
+                    <div class="news-info">
+                        <span>August 2, 2016</span>
+                        <a href="#">
+                            <h3>NAKUMATT ON BOARD- LET'S FIGHT THIS BATTLE TOGETHER</h3>
+                        </a>
+                        <p>
+                          In 2014, Nakumatt chose to partner Faraja to deliver 
+                          two ground-breaking projects called 'Make-Over' and 
+                          'Let's Fight this Battle Together'. Both show the 
+                          power corporate partnerships can have.
+                        </p>
+                        <p>
+                          In 2014, Nakumatt chose to partner Faraja to deliver 
+                          two ground-breaking projects called 'Make-Over' and 
+                          'Let's Fight this Battle Together'. Both show the 
+                          power corporate partnerships can have.
+                        </p>
+                        <a class="read-more" href="#">Read More  <FontAwesomeIcon icon={faAngleRight} /></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className='col-md-2'></div>
+              <div className='col-md-5'>
+                <div className='news-active'>
+                  <div className='single-blog d-flex align-items-center'>
+                  <div class="news-info">
+                        <span>July 13, 2016</span>
+                        <a href="#">
+                            <h3>WHAT IS KENYA'S BIGGEST COFFEE MORNING?</h3>
+                        </a>
+                        <p>
+                          If you’ve heard about Kenya’s Biggest Coffee 
+                          Morning – and want to find out more – you at the right place.
+                        </p>
+                        <a class="read-more" href="#">Read More  <FontAwesomeIcon icon={faAngleRight} /></a>
+                    </div>
+                    {/* <div className='thumb'>
+                      <Image className="img-thumb" src={FarajaCoffee} alt='Cancer Screening' />
+                    </div> */}
+                    
+                  </div>
                 </div>
               </div>
             </div>
