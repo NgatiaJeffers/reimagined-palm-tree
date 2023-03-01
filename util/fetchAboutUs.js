e5873db77a47c1f8b9f04471782bbcaa03b77239
Jefferson Gakuya <jeffersongakuya@gmail.com>
2023-03-01 10:42:42 +0300

    JG: Increase timeout

--- util/fetchAboutUs.js
+++ util/fetchAboutUs.js
@@ -1,6 +1,7 @@
 export const fetchAboutUs = async () => {
     const res = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/getAboutUs`
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/getAboutUs`,
+        {timeout: 60000}
     );
 
     if (!res.ok) return;
