f85169fc26d54554cae1e2754dfa45d477c3a9f9
Jefferson Gakuya <jeffersongakuya@gmail.com>
2023-05-08 17:08:35 +0300

    JG: Code clean up

--- components/shared/DonateSidebar/index.js
+++ components/shared/DonateSidebar/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-import icon from "../../public/hand.png";
+import icon from "../../../public/hand.png";
 
 const DonateSidebar = () => {
   return (
