a4508cf89c3f79bd5f0d80811580b23e686a8641
Jefferson Gakuya <jeffersongakuya@gmail.com>
2023-05-10 10:30:36 +0300

    JG[feat] Pass child components and props to parent

--- pages/support-us/index.js
+++ pages/support-us/index.js
@@ -1,20 +1,46 @@
 import React, { Fragment } from "react";
 import Link from "next/link";
-import BreadCrumb from "../../components/atoms/Breadcrumb";
 
+import BreadCrumb from "../../components/atoms/Breadcrumb";
+import Accordion from "../../components/atoms/Accordion"
+;
 const SupportUsPage = () => {
   return (
     <Fragment>
       <BreadCrumb title={"Support Us"} />
-      <section className="blog-area">
+      <section className="support-area">
         <div className="container">
-          <div className="blog-title text-center">
-            <div className="sub-title">
-              <div className="inner text-center">
-                <h3>Become a part of the community</h3>
+          <div className="row">
+            <div className="col-xl-6 col-12 col-lg-6 col-md-12">
+              <div className="content-box">
+                  <h2 className="heading-title">Become a part of the <span className="underline">community</span></h2>
+                  <p>
+                    By supporting Faraja you will become a part of the 
+                    community that is working hard to fight the major 
+                    cancer issues we are facing here in Kenya. 
+                    As we rely entirely on voluntary donations 
+                    and support, so by getting involved you will 
+                    make a huge difference.
+                  </p>
               </div>
             </div>
-            <h2>Support Us</h2>
+          </div>
+          <div className="row">
+            <div className="col-xl-6 col-12 col-lg-6 col-md-12">
+              <h2 className="sub-heading">The different ways you can get involved</h2>
+            </div>
+          </div>
+          <div className="row">
+          <div className="col-xl-6 col-12 col-lg-6 col-md-12">
+              <div className="content-box">
+                <Accordion panels={panels} />
+              </div>
+            </div>
+            <div className="col-xl-6 col-12 col-lg-6 col-md-12">
+              <div className="content-box">
+                <Accordion panels={panels} />
+              </div>
+            </div>
           </div>
         </div>
       </section>
@@ -23,3 +49,10 @@ const SupportUsPage = () => {
 };
 
 export default SupportUsPage;
+
+const panels = [
+  {
+    label: "Become a monthly regular giver",
+    content: "Just KSH 500 a month could help to provide paint, paper and brushes towards our Art Therapy classes for children with cancer."
+  }
+]
