fd91a4a2bcb6f4de2e5aba05232457332e1db1d2
Jefferson Gakuya <jeffersongakuya@gmail.com>
2023-05-11 15:38:30 +0300

    JG[feat] Add info on cards

--- pages/donate/index.js
+++ pages/donate/index.js
@@ -4,7 +4,9 @@ import Image from "next/image";
 import BreadCrumb from "../../components/atoms/Breadcrumb";
 import DonateForm from "../../components/Form/DonateForm";
 
-import mpesa from "../../public/mpesa.png"
+import mpesa from "../../public/mpesa.png";
+import amazon from "../../public/smile-amazon.png";
+import Link from "next/link";
 
 const DonatePage = () => {
   return (
@@ -37,11 +39,7 @@ const DonatePage = () => {
                 difference. It is only together that we can help fight cancer.
               </p>
               <div className="donation-wrap">
-                <Image 
-                  src={mpesa}
-                  width={100}
-                  height={70}
-                />
+                <Image src={mpesa} width={100} height={70} />
                 <p>
                   Use our secure M-pesa payment gateway to donate through our
                   paybill 509700. You will receive a pop up on your phone to
@@ -49,9 +47,143 @@ const DonatePage = () => {
                 </p>
                 <DonateForm />
               </div>
+              <div className="row amazon-donate-card d-lg-flex align-items-center top-broken-grid m-0">
+                <div className="col-md-4 amazon-img">
+                  <Image src={amazon} />
+                </div>
+                <div className="col-md-8 text">
+                  {/* <div className="d-md-flex align-items-center"> */}
+                    <p>
+                      This Year, you can do your Amazon Christmas shopping and
+                      support Faraja Cancer Support Trust at an extra cost to
+                      you.
+                    </p>
+                    <ol className="list-group list-group-numbered">
+                      <li className="list-group-item">
+                        Simply go to <Link href={""}><a className="underline">smile.amazon.co.uk</a></Link>
+                        on your web browser or activate AmazonSmile on your
+                        Amazon app (look in 'settings').
+                      </li>
+                      <li className="list-group-item">
+                        Then you select Faraja Cancer Support Trust UK from the
+                        list of charities... and get shoppings.
+                      </li>
+                    </ol>
+                    <p>
+                      100% of money raised in the U.K. goes to Faraja Cancer
+                      Support Trust Kenya. It provides vital funding for the
+                      Trust's free complementary therapies, support groups and
+                      financial assistance for cancer treatments.
+                    </p>
+                  {/* </div> */}
+                </div>
+              </div>
             </div>
             <div className="col-lg-4 col-md-12">
-              <div className="more-donate-info"></div>
+              <div className="more-donate-info">
+                <div className="card-big-shadow">
+                  <div className="card">
+                    <div className="content">
+                      <h3>CREDIT / DEBIT CARD DONATION</h3>
+                      <p>
+                        If you would like to pay by credit/debit card please
+                        click the button below.
+                      </p>
+                      <Link
+                        href={
+                          "https://www.globalgiving.org/projects/farajacancersupportfund/"
+                        }
+                      >
+                        <a className="btn-donate">DONATE VIA GLOBAL GIVING</a>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="card-big-shadow">
+                  <div className="card">
+                    <div className="content">
+                      <h3>CHEQUE OR BANK TRANSFER</h3>
+                      <p>
+                        You can send cheques or money orders payable to{" "}
+                        <span className="underline">
+                          Faraja Cancer Support Trust.
+                        </span>{" "}
+                        Direct Debits can be deposited into our bank accounts:
+                      </p>
+                    </div>
+                    <div className="banks-info">
+                      <h3>Kenya Shillings</h3>
+                      <ol className="list-group">
+                        <li className="list-group-item d-flex justify-content-between align-items-start">
+                          <div className="ms-2 me-auto">
+                            <div className="fw-bold">Bank</div>
+                            NCBA Bank Kenya PLC (Bank code 07000)
+                          </div>
+                        </li>
+                        <li className="list-group-item d-flex justify-content-between align-items-start">
+                          <div className="ms-2 me-auto">
+                            <div className="fw-bold">Branch</div>
+                            The Mall Branch (Branch code 105)
+                          </div>
+                        </li>
+                        <li className="list-group-item d-flex justify-content-between align-items-start">
+                          <div className="ms-2 me-auto">
+                            <div className="fw-bold">Account Name</div>
+                            Faraja Cancer Support Trust
+                          </div>
+                        </li>
+                        <li className="list-group-item d-flex justify-content-between align-items-start">
+                          <div className="ms-2 me-auto">
+                            <div className="fw-bold">Account Number</div>
+                            1525110178
+                          </div>
+                        </li>
+                        <li className="list-group-item d-flex justify-content-between align-items-start">
+                          <div className="ms-2 me-auto">
+                            <div className="fw-bold">SWIFT code</div>
+                            CBAFKENX
+                          </div>
+                        </li>
+                      </ol>
+
+                      <h3>US Dollars</h3>
+                      <ol className="list-group">
+                        <li className="list-group-item d-flex justify-content-between align-items-start">
+                          <div className="ms-2 me-auto">
+                            <div className="fw-bold">Bank</div>
+                            NCBA Bank Kenya PLC (Bank code 07000)
+                          </div>
+                        </li>
+                        <li className="list-group-item d-flex justify-content-between align-items-start">
+                          <div className="ms-2 me-auto">
+                            <div className="fw-bold">Branch</div>
+                            The Mall Branch (Branch code 105)
+                          </div>
+                        </li>
+                        <li className="list-group-item d-flex justify-content-between align-items-start">
+                          <div className="ms-2 me-auto">
+                            <div className="fw-bold">Account Name</div>
+                            Faraja Cancer Support Trust
+                          </div>
+                        </li>
+                        <li className="list-group-item d-flex justify-content-between align-items-start">
+                          <div className="ms-2 me-auto">
+                            <div className="fw-bold">Account Number</div>
+                            1525110199
+                          </div>
+                        </li>
+                        <li className="list-group-item d-flex justify-content-between align-items-start">
+                          <div className="ms-2 me-auto">
+                            <div className="fw-bold">SWIFT code</div>
+                            CBAFKENX
+                          </div>
+                        </li>
+                      </ol>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
